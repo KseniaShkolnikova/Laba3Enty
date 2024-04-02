6c127ed0be6245f1a9dacde27e7ad6f11cf1a4fa
KseniaShkolnikova <isip_k.v.shkolnikova@mpt.ru>
2024-04-02 10:59:11 +0300

    asdasd

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DATASET
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PRAKT3Entities context = new PRAKT3Entities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ingr.ItemsSource = context.SUSHI_STORE.ToList();
+        }
+    }
+}
